82ce5e05618e5c1d2ef57f4d11706ba5af4221fc
marouane <marwanmakhloufraja@gmail.com>
2024-06-18 22:31:38 +0100

    add all file

---
+++ script.js
@@ -0,0 +1,304 @@
+
+var sorahAarry = [
+    { id: 1, name: "الفاتحة" },
+    { id: 2, name: "البقرة" },
+    { id: 3, name: "آل عمران" },
+    { id: 4, name: "النساء" },
+    { id: 5, name: "المائدة" },
+    { id: 6, name: "الأنعام" },
+    { id: 7, name: "الأعراف" },
+    { id: 8, name: "الأنفال" },
+    { id: 9, name: "التوبة" },
+    { id: 10, name: "يونس" },
+    { id: 11, name: "هود" },
+    { id: 12, name: "يوسف" },
+    { id: 13, name: "الرعد" },
+    { id: 14, name: "إبراهيم" },
+    { id: 15, name: "الحجر" },
+    { id: 16, name: "النحل" },
+    { id: 17, name: "الإسراء" },
+    { id: 18, name: "الكهف" },
+    { id: 19, name: "مريم" },
+    { id: 20, name: "طه" },
+    { id: 21, name: "الأنبياء" },
+    { id: 22, name: "الحج" },
+    { id: 23, name: "المؤمنون" },
+    { id: 24, name: "النور" },
+    { id: 25, name: "الفرقان" },
+    { id: 26, name: "الشعراء" },
+    { id: 27, name: "النمل" },
+    { id: 28, name: "القصص" },
+    { id: 29, name: "العنكبوت" },
+    { id: 30, name: "الروم" },
+    { id: 31, name: "لقمان" },
+    { id: 32, name: "السجدة" },
+    { id: 33, name: "الأحزاب" },
+    { id: 34, name: "سبأ" },
+    { id: 35, name: "فاطر" },
+    { id: 36, name: "يس" },
+    { id: 37, name: "الصافات" },
+    { id: 38, name: "ص" },
+    { id: 39, name: "الزمر" },
+    { id: 40, name: "غافر" },
+    { id: 41, name: "فصلت" },
+    { id: 42, name: "الشورى" },
+    { id: 43, name: "الزخرف" },
+    { id: 44, name: "الدخان" },
+    { id: 45, name: "الجاثية" },
+    { id: 46, name: "الأحقاف" },
+    { id: 47, name: "محمد" },
+    { id: 48, name: "الفتح" },
+    { id: 49, name: "الحجرات" },
+    { id: 50, name: "ق" },
+    { id: 51, name: "الذاريات" },
+    { id: 52, name: "الطور" },
+    { id: 53, name: "النجم" },
+    { id: 54, name: "القمر" },
+    { id: 55, name: "الرحمن" },
+    { id: 56, name: "الواقعة" },
+    { id: 57, name: "الحديد" },
+    { id: 58, name: "المجادلة" },
+    { id: 59, name: "الحشر" },
+    { id: 60, name: "الممتحنة" },
+    { id: 61, name: "الصف" },
+    { id: 62, name: "الجمعة" },
+    { id: 63, name: "المنافقون" },
+    { id: 64, name: "التغابن" },
+    { id: 65, name: "الطلاق" },
+    { id: 66, name: "التحريم" },
+    { id: 67, name: "الملك" },
+    { id: 68, name: "القلم" },
+    { id: 69, name: "الحاقة" },
+    { id: 70, name: "المعارج" },
+    { id: 71, name: "نوح" },
+    { id: 72, name: "الجن" },
+    { id: 73, name: "المزمل" },
+    { id: 74, name: "المدثر" },
+    { id: 75, name: "القيامة" },
+    { id: 76, name: "الإنسان" },
+    { id: 77, name: "المرسلات" },
+    { id: 78, name: "النبأ" },
+    { id: 79, name: "النازعات" },
+    { id: 80, name: "عبس" },
+    { id: 81, name: "التكوير" },
+    { id: 82, name: "الإنفطار" },
+    { id: 83, name: "المطففين" },
+    { id: 84, name: "الإنشقاق" },
+    { id: 85, name: "البروج" },
+    { id: 86, name: "الطارق" },
+    { id: 87, name: "الأعلى" },
+    { id: 88, name: "الغاشية" },
+    { id: 89, name: "الفجر" },
+    { id: 90, name: "البلد" },
+    { id: 91, name: "الشمس" },
+    { id: 92, name: "الليل" },
+    { id: 93, name: "الضحى" },
+    { id: 94, name: "الشرح" },
+    { id: 95, name: "التين" },
+    { id: 96, name: "العلق" },
+    { id: 97, name: "القدر" },
+    { id: 98, name: "البينة" },
+    { id: 99, name: "الزلزلة" },
+    { id: 100, name: "العاديات" },
+    { id: 101, name: "القارعة" },
+    { id: 102, name: "التكاثر" },
+    { id: 103, name: "العصر" },
+    { id: 104, name: "الهمزة" },
+    { id: 105, name: "الفيل" },
+    { id: 106, name: "قريش" },
+    { id: 107, name: "الماعون" },
+    { id: 108, name: "الكوثر" },
+    { id: 109, name: "الكافرون" },
+    { id: 110, name: "النصر" },
+    { id: 111, name: "المسد" },
+    { id: 112, name: "الإخلاص" },
+    { id: 113, name: "الفلق" },
+    { id: 114, name: "الناس" }
+];
+var ayahs = [
+    { id: 1, ayahs: 7 },
+    { id: 2, ayahs: 286 },
+    { id: 3, ayahs: 200 },
+    { id: 4, ayahs: 176 },
+    { id: 5, ayahs: 120 },
+    { id: 6, ayahs: 165 },
+    { id: 7, ayahs: 206 },
+    { id: 8, ayahs: 75 },
+    { id: 9, ayahs: 129 },
+    { id: 10, ayahs: 109 },
+    { id: 11, ayahs: 123 },
+    { id: 12, ayahs: 111 },
+    { id: 13, ayahs: 43 },
+    { id: 14, ayahs: 52 },
+    { id: 15, ayahs: 99 },
+    { id: 16, ayahs: 128 },
+    { id: 17, ayahs: 111 },
+    { id: 18, ayahs: 110 },
+    { id: 19, ayahs: 98 },
+    { id: 20, ayahs: 135 },
+    { id: 21, ayahs: 112 },
+    { id: 22, ayahs: 78 },
+    { id: 23, ayahs: 118 },
+    { id: 24, ayahs: 64 },
+    { id: 25, ayahs: 77 },
+    { id: 26, ayahs: 227 },
+    { id: 27, ayahs: 93 },
+    { id: 28, ayahs: 88 },
+    { id: 29, ayahs: 69 },
+    { id: 30, ayahs: 60 },
+    { id: 31, ayahs: 34 },
+    { id: 32, ayahs: 30 },
+    { id: 33, ayahs: 73 },
+    { id: 34, ayahs: 54 },
+    { id: 35, ayahs: 45 },
+    { id: 36, ayahs: 83 },
+    { id: 37, ayahs: 182 },
+    { id: 38, ayahs: 88 },
+    { id: 39, ayahs: 75 },
+    { id: 40, ayahs: 85 },
+    { id: 41, ayahs: 54 },
+    { id: 42, ayahs: 53 },
+    { id: 43, ayahs: 89 },
+    { id: 44, ayahs: 59 },
+    { id: 45, ayahs: 37 },
+    { id: 46, ayahs: 35 },
+    { id: 47, ayahs: 38 },
+    { id: 48, ayahs: 29 },
+    { id: 49, ayahs: 18 },
+    { id: 50, ayahs: 45 },
+    { id: 51, ayahs: 60 },
+    { id: 52, ayahs: 49 },
+    { id: 53, ayahs: 62 },
+    { id: 54, ayahs: 55 },
+    { id: 55, ayahs: 78 },
+    { id: 56, ayahs: 96 },
+    { id: 57, ayahs: 29 },
+    { id: 58, ayahs: 22 },
+    { id: 59, ayahs: 24 },
+    { id: 60, ayahs: 13 },
+    { id: 61, ayahs: 14 },
+    { id: 62, ayahs: 11 },
+    { id: 63, ayahs: 11 },
+    { id: 64, ayahs: 18 },
+    { id: 65, ayahs: 12 },
+    { id: 66, ayahs: 12 },
+    { id: 67, ayahs: 30 },
+    { id: 68, ayahs: 52 },
+    { id: 69, ayahs: 52 },
+    { id: 70, ayahs: 44 },
+    { id: 71, ayahs: 28 },
+    { id: 72, ayahs: 28 },
+    { id: 73, ayahs: 20 },
+    { id: 74, ayahs: 56 },
+    { id: 75, ayahs: 40 },
+    { id: 76, ayahs: 31 },
+    { id: 77, ayahs: 50 },
+    { id: 78, ayahs: 40 },
+    { id: 79, ayahs: 46 },
+    { id: 80, ayahs: 42 },
+    { id: 81, ayahs: 29 },
+    { id: 82, ayahs: 19 },
+    { id: 83, ayahs: 36 },
+    { id: 84, ayahs: 25 },
+    { id: 85, ayahs: 22 },
+    { id: 86, ayahs: 17 },
+    { id: 87, ayahs: 19 },
+    { id: 88, ayahs: 26 },
+    { id: 89, ayahs: 30 },
+    { id: 90, ayahs: 20 },
+    { id: 91, ayahs: 15 },
+    { id: 92, ayahs: 21 },
+    { id: 93, ayahs: 11 },
+    { id: 94, ayahs: 8 },
+    { id: 95, ayahs: 8 },
+    { id: 96, ayahs: 19 },
+    { id: 97, ayahs: 5 },
+    { id: 98, ayahs: 8 },
+    { id: 99, ayahs: 8 },
+    { id: 100, ayahs: 11 },
+    { id: 101, ayahs: 11 },
+    { id: 102, ayahs: 8 },
+    { id: 103, ayahs: 3 },
+    { id: 104, ayahs: 9 },
+    { id: 105, ayahs: 5 },
+    { id: 106, ayahs: 4 },
+    { id: 107, ayahs: 7 },
+    { id: 108, ayahs: 3 },
+    { id: 109, ayahs: 6 },
+    { id: 110, ayahs: 3 },
+    { id: 111, ayahs: 5 },
+    { id: 112, ayahs: 4 },
+    { id: 113, ayahs: 5 },
+    { id: 114, ayahs: 6 }
+];
+
+
+const sorah = document.getElementById('sorah');
+sorah.innerHTML = sorahAarry.map(s=>{
+    if(s.id === 1) return `<option selected="true" value="${s.id}">${s.name} </option>`
+    return `<option value="${s.id}">${s.name} </option>`
+})
+const ayah = document.getElementById('ayah');
+
+function selectOption (s){
+    main.innerHTML = `
+    <div class="ici">
+    <div class="typewriter">
+  <div class="slide"><i></i></div>
+  <div class="paper"></div>
+  <div class="keyboard"></div>
+</div>
+</div>
+`
+    if(s === 'all'){
+        getData(true);
+    }
+}
+
+function getData(i) {
+    // Use Promise.all to wait for both axios requests to complete
+    Promise.all([
+        axios.get("./quran.json"),
+        axios.get("./tafseer.json")
+    ]).then(([qurand, tafsird]) => {
+        // Call showData after both promises are resolved
+        showData(i, qurand, tafsird);
+    }).catch(error => {
+        console.error("Error fetching data", error);
+    });
+}
+
+
+const main = document.getElementById('main')
+const showData = (i, q, t) => {
+    console.log(q);
+    console.log(t);
+
+    if (i) {
+        main.innerHTML = sorahAarry.map(s => {
+            return `
+                <div class="opt">
+                    <h3 style="background:#00000022;">${s.name}</h3>
+                    ${
+                        q.data[s.id - 1].array.map(item => {
+                            const tafsirText = t.data.find(taf => taf.number == s.id && taf.aya == item.id)?.text || 'تفسير غير موجود';
+                            return `<div style="background: #00000033" class="opt ayaht">
+                                <p id="a${s.id}" style="background:orange;color:#000;" class="opt ayah">${item.ar}</p>
+                                <p id="t${s.id}" class="tafsir">${tafsirText}</p>
+                            </div>`;
+                        }).join('')
+                    }
+                </div>
+            `;
+        }).join('');
+    }
+};
+
+
+
+
+
+
+
+
+
